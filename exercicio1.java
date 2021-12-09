aaf35bb58faf97fa40d6f234e2e9fefa1da7b4c4
Vicky-Silva <vickyechelon2105@gmail.com>
2021-12-08 21:38:56 -0300

    exercicio

---
+++ exercicio1.java
@@ -0,0 +1,26 @@
+package exeGeneration;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+	       	Scanner leia = new Scanner(System.in);
+		
+		
+	int anos,meses,dias,totalDias;
+	
+	System.out.println("\nEntre quantos anos viveu: ");
+	anos=leia.nextInt();
+	System.out.println("\nEntre quantos meses viveu: ");
+	meses=leia.nextInt();
+	System.out.println("\nEntre quantos dias viveu: ");
+	dias=leia.nextInt();
+	
+	totalDias = anos*365 + meses*30 + dias;
+	
+	System.out.println("\nVocê viceu "+totalDias+" dia(s)");
+	
+	}
+	
+}
